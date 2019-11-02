f67295eafa97daf9f849b5e9b17058b98581717b
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2019-11-01 18:09:28 -0600

    feat: change report back to google doc

--- compress.sh
+++ compress.sh
@@ -3,4 +3,8 @@
 set -e
 set -u
 
-tar -czf prjcode.tgz registry.py README.txt Report.pdf
+tar -czf prjcode.tgz                     \
+    registry.py                          \
+    registry_agent.py traffic_officer.py \
+    input_util.py                        \
+    README.txt Report.pdf
