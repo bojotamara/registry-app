11fcf6081a92440b04300d8ea25692734790a345
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2019-11-04 14:50:42 -0700

    fix: person marry himself

--- registry_agent.py
+++ registry_agent.py
@@ -133,6 +133,10 @@ class RegistryAgent:
         )
         p2_last_name = input_util.read_name("Please enter second partner's last name: ")
 
+        if p1_first_name.lower() == p2_first_name.lower and p1_last_name.lower() == p2_last_name.lower():
+            print("A person cannot marry him or herself")
+            return;
+
         partner_1 = self.__get_person(p1_first_name, p1_last_name)
         if partner_1 is None:
             print(
